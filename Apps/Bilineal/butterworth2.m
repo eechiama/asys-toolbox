e5a73053e9e9c60110d8b0bf2de68ee1b7620d1a
Andres Hojnadel <a.hojnadel@gmail.com>
2020-06-29 11:08:33 -0300

    Aplicacion de filtro por transformada bilineal - Apps-> bilineal

--- Apps/Bilineal/butterworth2.m
+++ Apps/Bilineal/butterworth2.m
@@ -3,22 +3,24 @@ clc, clear, close all
 
 %% Cargo un .mpr
 save_name = 'turn.mp3';             % Turn the page - Metallica
-[x, fs]=audioread(save_name);       % Carga el audio: x es el audio, fs la frecuencia de muestreo
+[x, fs] = audioread(save_name);     % Carga el audio: x es el audio, fs la frecuencia de muestreo
 %sound(ft, Fs);                     % Reproduce el audio original
 %pause;
 
 % Creación de vector de tiempo para graficar
-t=0:1/fs:(length(x)-1)/fs;          % Vector de tiempo
+t = 0:1/fs:(length(x)-1)/fs;        % Vector de tiempo
 T = 1/fs;                           % Intervalo de tiempo de muestreo
 
-fw=fft(x)/length(x);                % Espectro de la canción original
+fw = fft(x)/length(x);              % Espectro de la canción original
 
 % Creación del vector de frecuencias
 N = floor(length(fw)/2);            % Cantidad de muestras de medio espectro
-f=(0:N-1)*fs/2/N;                   % Vector de frecuencias
+f = (0:N-1)*fs/2/N;                 % Vector de frecuencias
 
 figure, plot(f,abs(fw(1:N))), grid
 title('Espectro del audio original'), xlabel('Frecuencia [Hz]')
+
+disp(['Frecuencia de muestreo (fs) = ',num2str(fs)]);
 
 
 %% Creo Sistema ANALOGICO
@@ -27,64 +29,58 @@ wc = fc*2*pi;                   % Omega de corte
 
 % Seleccionando uno de los 2 numeradores se consigue un pasa-bajos o un
 % pasa-altos. Jugar también con la frecuencia de corte fc definida antes
-
-%num = [1 0 0];                   % Pasa-Altos;  fc recomendada 5kHz
-num = (wc)^2;                  % Pasa-Bajos;  fc recomendada 1kHz
-den = [1 sqrt(2)*wc wc^2];
-Hs = tf(num,den);                 % Función transferencia de Butterworth de 2do orden
-
-% Diagrama de polos y ceros
-figure,pzmap(Hs), title('PZMAP, H(s)'), grid, axis([-10000 0 -10000 10000])
-a=findobj(gca,'type','line'); set(a(2:end),'linewidth',3), set(a(2:end),'markersize',10),
+% num = [1 0 0];                  % Pasa-Altos;  fc recomendada 5kHz
+num = (wc)^2;                   % Pasa-Bajos;  fc recomendada 1kHz
+den = [1 sqrt(2)*wc wc^2];      % Denominador de un butterworth 2do orden
+Hs = tf(num,den)                % Función transferencia de Butterworth de 2do orden
 
 % Bode
 options = bodeoptions; options.FreqUnits = 'Hz';
-figure, bode(Hs, options)
+figure, bode(Hs, options), grid
 
 % Respuesta al Impulso
-figure, impulse(num,den), title('h(t)')
+figure, impulse(num,den), title('h(t)'), grid
 
-
-%%
-%Obtengo Wc DIGITAL;
-omegac = wc/fs;                               % Omega de corte "esperada"
-omegac_bil = 2*atan(omegac/2);                % Omega de corte real bilineal
-fc_bil = omegac_bil *fs/2/pi;
-disp (['Frecuencia de corte ideal: ' num2str(fc)])
-disp (['Frecuencia de corte bilineal: ' num2str(fc_bil)])
-
-%% Calculo Bilineal -> Paso de H(s) ->H(z)
-[numz, denz] = bilinear(num,den,fs);        % Transformada bilineal
-Hz = tf(numz,denz,1/fs);                      % Sistema digital
-
-% Diagrama Polos y Ceros
-figure,pzmap(Hz), title('PZ H(z)')
+% Diagrama de polos y ceros
+figure, pzmap(Hs), title('Diagrama de polos y ceros H(s)'), grid, axis([-10000 0 -10000 10000])
 a=findobj(gca,'type','line'); set(a(2:end),'linewidth',3), set(a(2:end),'markersize',10),
 
+
+%% Calculo Bilineal -> Paso de H(s) ->H(z)
+[numz, denz] = bilinear(num,den,fs);            % Transformada bilineal
+Hz = tf(numz,denz,1/fs)                         % Sistema digital
+
 % Respuesta al Impulso
-figure(6),impz(num,den,fs), title('h[n]')
+[hn, nn] = impulse(Hz);
+figure(6); title('h[n]'), stem(nn, hn);
+
+% Diagrama Polos y Ceros
+figure ,pzmap(Hz), title('Diagrama de polos y ceros H(z)')
+a = findobj(gca,'type','line'); set(a(2:end),'linewidth',3), set(a(2:end),'markersize',10),
+
+
+%% Obtengo Wc DIGITAL;
+Omegac = wc/fs;                               % Omega de corte "esperada"
+Omegac_bil = 2*atan(Omegac/2);                % Omega de corte real bilineal
+fc_bil = Omegac_bil *fs/2/pi;                 % Frecuencia de corte obtenida al aplicar Bilineal
+
+disp (['Frecuencia de corte ideal/esperada: ' num2str(fc)])
+disp (['Frecuencia de corte bilineal: ' num2str(fc_bil)])
+
 
 %% Grafico y Filtro digital
 
-% Rta en frecuencia (lineal)
-[HHs, ws]=freqs(num,den);
-figure, title('|H(s)| y \phi(H(s))')
-subplot(211),plot(ws/2/pi,abs(HHs)),axis tight,xlabel('Frecuencia [Hz]'), ylabel('|H(w)|')
-subplot(212),plot(ws/2/pi,angle(HHs)),axis tight,xlabel('Frecuencia [Hz]'), ylabel('\phi\{H(w)\}')
+[HHs, ws] = freqs(num,den);                   % Rta en frecuencia del filtro analógico
+[HHz, wz] = freqz(numz,denz);                 % Rta en frecuencia del filtro digital
 
-% Respuesta en frecuencia digital
-[HHz, wz]=freqz(numz,denz);
-figure(5), title('|H(z)| y \phi\{H(z)\}')
-subplot(211),plot(wz,abs(HHz)),axis tight,xlabel('Frecuencia angular [1/s]'), ylabel('|H(w)|'), grid
-subplot(212),plot(wz,angle(HHz)),axis tight,xlabel('Frecuencia angular [1/s]'), ylabel('\phi\{H(z)\}'), grid
-
-figure,
-plot(ws/2/pi,abs(HHs),wz*fs/2/pi,abs(HHz))
+figure
+plot(ws/2/pi, abs(HHs), '--b', wz*fs/2/pi, abs(HHz),'r','linewidth',2),  grid
+legend('Filtro analógico','Filtro digital')
 
 %%
 % Filtro el audio
-y=filter(numz,denz,x);                  % Audio filtrado con filtro digital
-fy = fft(y)/length(y);                  % Espectro del audio filtrado
+y = filter(numz,denz,x);                  % Audio filtrado con filtro digital
+fy = fft(y)/length(y);                    % Espectro del audio filtrado
 
 figure,
 ax2 = subplot(211), plot(f,abs(fw(1:length(y)/2))), grid
