3a80c3f9af0e78bcb9570916c869b03398999091
Esteban Eduardo Chiama <eechiama@gmail.com>
2020-11-30 13:56:13 -0300

    agregada función dsierra() para generar una señal diente de sierra

---
+++ Señales/dsierra.m
@@ -0,0 +1,65 @@
+% dsierra -- Genera la señal diente de sierra y su vector de tiempo correspondiente.
+%
+%	ft = dsierra(T0, ciclos, dt)
+%	ft = dsierra(T0, ciclos, dt, start)
+%
+% Argumentos
+% ==========
+%
+%	T0:	período deseado de la señal generada.
+%	ciclos: ciclos deseados de la señal generada.
+%	dt:	paso temporal para el vector de tiempos.
+%	start:	determina en qué valores de tiempo comienza la señal.
+%
+% Retorna
+% =======
+%
+%	xt:	vector de valores de la señal generada.
+%	t:	vector temporal generado.
+%
+% Detalle
+% =======
+%
+% El 4to argumento ('start') es opcional. Sus posibles valores son:
+%
+%	'cero': comienza en t=0. (default)
+%	
+%	'neg+': comienza en valores de tiempo negativos.
+%		si 'ciclos' es impar, habrá un ciclo más en tiempos positivos.
+%
+%	'neg-': comienza en valores negativos.
+%		si 'ciclos' es impar, habrá un ciclo más en tiempos negativos.
+%
+% Si la cantidad de ciclos especificada es un número entero entonces en t=0 siempre se dará el
+% comienzo de un nuevo ciclo. Esto es sin importar qué valor de 'start' se haya especificado.
+%
+% Si la cantidad de ciclos especificada no es un número entero, entonces ya no necesariamente
+% en t=0 comenzará un ciclo.
+%
+function [xt, t] = dsierra(T0,ciclos,dt,start)
+
+if nargin == 3
+	start = 'cero';
+end
+
+if !strcmp(start,'cero')
+  ciclos_der = floor(ciclos/2);
+	ciclos_izq = ciclos_der + mod(ciclos,2);
+	if start(end)=='+'
+		aux = ciclos_izq;
+		ciclos_izq = ciclos_der;
+		ciclos_der = aux;
+	end
+	nizq = -ciclos_izq;
+	nder = ciclos_der;
+	t = -ciclos_izq*T0 : dt : ciclos_der*T0 - dt;
+else
+	t = 0 : dt : (ciclos*T0) - dt;
+	nizq = 0;
+	nder = ciclos;
+end
+
+xt = 0;
+for n = nizq:nder;
+	xt = xt + rampa(t-T0*n) - rampa(t-T0*(n+1)) - T0*escalon(t-T0*(n+1));
+end
